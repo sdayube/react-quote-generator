9fd25a4981be963bbb6ceaf6bfc1d2cff77d324f
Silvio Dayube <scarige@gmail.com>
2021-02-12 16:04:06 -0300

    Creates App component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quotes: [],
+      quote: {},
+    }
+    this.changeQuote = this.changeQuote.bind(this);
+  }
+
+  selectQuote(quotes) {
+    const index = Math.floor(Math.random() * quotes.length);
+    return quotes[index];
+  }
+
+  async componentDidMount() {
+    fetch("https://type.fit/api/quotes")
+      .then((response) => response.json())
+      .then((quotes) => this.setState({
+        quotes,
+        quote: this.selectQuote(quotes),
+      }));
+  }
+
+  changeQuote() {
+    this.setState({
+      quote: this.selectQuote(this.state.quotes)
+    });
+  }
+
+  render() {
+    const { quotes, quote } = this.state;
+    if (!quotes.length) {
+      return (
+        <div>
+          <p>Loading...</p>
+        </div>
+      );
+    }
+    return (
+      <article id="quote-box">
+        <figure>
+          <blockquote id="text">{quote.text}</blockquote>
+          <figcaption id="author">{quote.author}</figcaption>
+        </figure>
+        <a href="https://twitter.com/intent/tweet" id="tweet-quote">Tweet quote</a>
+        <button type="button" id="new-quote" onClick={this.changeQuote}>New quote</button>
+      </article>
+    );
+  }
 }
 
 export default App;
